9e60e0b552bfb95afd8716e94003b0e4716ea2ff
olvap <oldani.pablo@gmail.com>
2015-06-03 21:09:28 -0300

    Update ruby.

--- ruby/install.sh
+++ ruby/install.sh
@@ -4,6 +4,6 @@ echo "  Installing rvm for you."
 
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 
-apt-get install curl
+sudo apt-get install curl
 
 \curl -sSL https://get.rvm.io | bash -s stable
