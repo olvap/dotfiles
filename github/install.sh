aa85e0a7b89a01cfc09698361a16b28069c8920e
olvap <oldani.pablo@gmail.com>
2015-06-03 21:10:20 -0300

    Update githun install script.

--- github/install.sh
+++ github/install.sh
@@ -1,8 +1,8 @@
-echo 'What is your github email'
-
-read -e gtihub_email
+read -p 'What is your github email: ' github_email
 
 ssh-keygen -t rsa -b 4096 -C $github_email
+
+sudo apt-get install xclip
 
 xclip ~/.ssh/id_rsa.pub
 
