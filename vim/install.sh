9997960607a094a6977d0e843fa2229212821009
olvap <oldani.pablo@gmail.com>
2015-11-08 18:34:40 -0300

    Add powerline fonts.

--- vim/install.sh
+++ vim/install.sh
@@ -2,6 +2,10 @@ echo "Installing vim"
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+# power line.
+git clone git@github.com:powerline/fonts.git
+./fonts/install.sh
+
 i vim
 
 vim +PluginInstall +qall
